d5b23f65730327d23660581e4255faa1c600b12c
yanlibo2021 <yanlibo2021@163.com>
2020-03-20 18:07:42 +0800

    modified:   server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
+
 server.use(middlewares);
 // server.use(router);
 // Add this before server.use(router)
